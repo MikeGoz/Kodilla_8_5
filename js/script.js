d02339737629498eaa49100ee095a8362e137e8e
MikeGoz <gozdz@onet.pl>
2017-04-21 14:20:48 +0200

    Task_8_5

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Kodilla task 8.5
+
+var a = prompt("Wpisz długość podstawy trójąta", 10),
+    h = prompt("Wpisz wysokość trójąta", 10),
+    triangleArea;
+
+if (a > 0 && h > 0) {
+
+   triangleArea = a * h / 2
+   console.log('Pole trójkąta o podstawie a= ' + a + ' i wysokości h= ' + h + ' wynosi: ' + triangleArea);
+   
+}
+
+else {
+   alert ('Wpisz poprawną wartość');
+}
